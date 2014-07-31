cec62dd95bf51b56c2e3a95928ecb8c58b8abaf9
gitgong <saltyfog@gmail.com>
2014-07-30 21:54:43 -0700

    fixed demo error

--- src/com/blake/where/ClockWorkMainActivity.java
+++ src/com/blake/where/ClockWorkMainActivity.java
@@ -205,7 +205,7 @@ here are the results from calling whereactivity for gps coordinates
     private void setUIClockedIn(){
         statusColor.setImageResource(R.drawable.green_square);
         statusColor.setVisibility(View.VISIBLE);
-        welcome.setText("ClockZ In");
+        welcome.setText("Clocked In");
         welcome.setTextColor(Color.GREEN);
         welcome.setVisibility(View.VISIBLE);
         login.setVisibility(View.INVISIBLE);
@@ -216,7 +216,7 @@ here are the results from calling whereactivity for gps coordinates
     private void setUIClockedOut(){
         statusColor.setImageResource(R.drawable.red_square);
         statusColor.setVisibility(View.VISIBLE);
-        welcome.setText("ClockZ Out");
+        welcome.setText("Clocked Out");
         welcome.setTextColor(Color.RED);
         welcome.setVisibility(View.VISIBLE);
         logout.setVisibility(View.INVISIBLE);
